d8dfea7a5f02c928a78e72759a3621963e27cdde
Jhon <jhon26vasquezm@gmail.com>
2026-02-24 10:29:22 -0500

    Clase_6_Agenda_ADSO_v4_Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -30,14 +30,15 @@ export default function App() {
   };
 
   return (
-    <main className="app-container">
+     <main className="max-w-2xl mx-auto mt-10 p-4">
 
-      <h1 className="app-title">Agenda ADSO v3</h1>
+       <h1 className="text-3xl font-bold text-morado text-center mb-2">
+        Agenda ADSO v4
+      </h1>
 
-      <p className="subtitulo">
-        Persistencia con localStorage + React
+      <p className="text-gray-500 text-center mb-6">
+        Interfaz moderna con TailwindCSS
       </p>
-
       <FormularioContacto onAgregar={agregarContacto} />
 
       {contactos.map((contacto) => (
